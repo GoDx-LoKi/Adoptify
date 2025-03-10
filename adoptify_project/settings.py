44c333d82599539f1fa206cc7b205f79760f91a4
dev-X-pvt <mobiboy278@gmail.com>
2025-03-11 00:46:10 +0530

    setting changed

--- adoptify_project/settings.py
+++ adoptify_project/settings.py
@@ -60,8 +60,9 @@ INSTALLED_APPS = [
     'pages',
     'inquiries',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
